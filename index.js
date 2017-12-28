ee596926763550b36d9e086b1c8cf3a934c6e351
Max Campbell <max@max-laptop.maxc.in>
2017-12-28 12:20:02 +0000

    removed support for tracert

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const os = require('os');
 
-const tracert = require('./tracert');
 const traceroute = require('./traceroute');
 
-module.exports = os.platform() === 'win32' ? tracert : traceroute;
+module.exports = traceroute;
